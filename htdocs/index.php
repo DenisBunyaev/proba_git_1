5aec6f8d336953bf5034f4bbdf74ba8daf1c2da9
Denis Olegovich <deniska@example.ua>
2018-10-30 16:10:22 +0200

    commit from Master branch

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,4 +1,19 @@
-﻿<?php
+﻿<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>The Master branch of this site</title>
+  <meta name="DenisKA" content="MySite">
+  <link rel="stylesheet" href="css/styles.css">
+  <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+</head>
+<body>
+  <script src="js/scripts.js"></script>
+<?php
 	echo "Privet ot Denisa<br/><hr>";
 	echo php.info;
 ?>
+</body>
+</html>
