25d0a91ab260075f24b03d1e7977b7a59d6be2fc
Denis Olegovich <deniska@example.ua>
2018-10-30 15:59:58 +0200

    commit from nwe_feature

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,4 +1,21 @@
-﻿<?php
-	echo "Privet ot Denisa<br/><hr>";
-	echo php.info;
-?>
+﻿<!doctype html>
+<html>
+	<head>
+	  <meta charset=\"utf-8\">
+	  <title>Some site</title>
+	  <meta name=\"description\" content=\"My test site for Git\">
+	  <meta name=\"DenisKA\" content=\"\">
+	  <link rel=\"stylesheet\" href=\"css/styles.css\">
+	  <!--[if lt IE 9]>
+		<script src=\"http://html5shiv.googlecode.com/svn/trunk/html5.js\"></script>
+	  <![endif]-->
+	</head>
+	<body>
+	  <script src=\"js/scripts.js\"></script>
+		<?php
+			echo "Privet ot Denisa<br/><hr>";
+			echo php.info;
+		?>
+	</body>
+</html>
+
